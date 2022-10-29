edcc476473044f2080a4a5181592620d927efc08
littlephillips <phillipkinuthia72@gmail.com>
2022-10-29 14:58:07 +0300

    class methods

--- lib/song.rb
+++ lib/song.rb
@@ -4,27 +4,6 @@ class Song
     @@count = 0
     @@artists = []
     @@genres = []
-  
-    def initialize(name, artist, genre)
-      @name = name
-      @artist = artist
-      @genre = genre
-      @@count += 1
-      @@genres << genre
-      @@artists << artist
-    end
-
-
-
-
-
-
-
-
-
-
-
-
 
   def initialize(name, artist, genre)
       @name = name
